175c694c7ef674a413022b21a6f0b97529c95aeb
Kendrick Gardner <kbgar24@gmail.com>
2018-05-14 21:21:04 -0500

    Configure Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const DIST_DIR = path.join(__dirname, 'dist');
+const CLIENT_DIR = path.join(__dirname, 'src');
+
+module.exports = {
+  context: CLIENT_DIR,
+
+  entry: {
+    app: './app',
+  },
+
+  output: {
+    path: `${DIST_DIR}/bundle`,
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/,
+    }, {
+      loader: 'css-loader',
+      test: /\.css/,
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: 'file-loader',
+      include: DIST_DIR
+    }]
+  },
+
+  devtool: 'cheap-module-eval-source-map',
+
+  devServer: {
+    contentBase: DIST_DIR,
+  },
+};
